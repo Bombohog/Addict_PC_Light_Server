cce8186a2c9b9af2685a64797f5c49f3262117b7
Anders Hjordrup Larsen <70882629+Hjordrup@users.noreply.github.com>
2021-09-10 10:47:24 +0200

    Create clientTest.java
    added a client test

---
+++ Client/clientTest.java
@@ -0,0 +1,30 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class MyClient {
+
+    public static void main(String[] args) throws IOException {
+
+        try {
+
+            Socket socket = new Socket("10.200.130.31", 8001);
+
+            DataInputStream inputStream = new DataInputStream(socket.getInputStream());
+         
+            // Scanner sc = new Scanner(System.in);
+            while (true) {
+                 
+              String temp = inputStream.readByte(); 
+              String humi = inputStream.readByte(); 
+               
+              System.out.println(temp)
+              System.out.println(humi)
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
